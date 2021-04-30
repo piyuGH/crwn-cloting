94eafbdb813518e8930999b9de6c5c72262479d3
piyuGH <piyushmpatel@live.com>
2021-04-30 11:35:28 +0530

    moved directory and shop data into its own reducer, created respective selectors and updated corresponding components with new rudux flow for directory/shop

---
+++ src/redux/shop/shop.selector.js
@@ -0,0 +1,27 @@
+import { createSelector } from "reselect";
+
+// const COLLECTION_ID_MAP = {
+//   hats: 1,
+//   sneackers: 2,
+//   jackets: 3,
+//   womens: 4,
+//   mens: 5,
+// };
+
+const selectShop = (state) => state.shop;
+
+export const selectCollections = createSelector(
+  [selectShop],
+  (shop) => shop.collections
+);
+// collectionUrlParam is a paramater
+export const selectCollection = (collectionUrlParam) =>
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]  //collection is object {} : data normalization thereby optimise query search
+    // when collections was []
+    // (collections) =>
+    //   collections.find(
+    //     (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    //   )
+  );
